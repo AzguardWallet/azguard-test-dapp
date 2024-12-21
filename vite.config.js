9e704dcaf06af5e398f80d2a149f1ccfcd355fc2
GussevPM <GussevPM@GMail.Com>
2024-12-21 21:40:13 +0100

    Change vite cfg

--- vite.config.js
+++ vite.config.js
@@ -13,4 +13,9 @@ export default defineConfig({
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
 		},
 	},
+	build: {
+		rollupOptions: {
+			external: ['@codemirror/state']
+		},
+	},
 })
